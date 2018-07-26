474f00617b2aa459631ff4c6b39fcf6d036bbe50
J Stewart <jstewart@asteriusmedia.com>
2018-07-26 14:39:09 +0000

    updated

--- diags/SaaSnodeslinks.js
+++ diags/SaaSnodeslinks.js
@@ -5,18 +5,23 @@ var nodeDataArray = [
 //
 // Common entities
 //
-    { "group": "0", "key": "Thing", "text": "Things", "description": "Things Entity", "color": "lime" },
-
+    
     // { "group": "1", "key": "Organization", "text": "Orgs", "description": "Organization Entity", "color": "orange", "parent": "thing" },
 
     // { "group": "1", "key": "Person", "text": "Persons", "description": "Person Entity - A Person entity is the contact information, role status, profile information and profile completion status of an individual in a role in a group. An User can be associated with more than one role profile and therefore more than one person in a group or in multiple groups.", "color": "orange", "parent": "thing" },
     // { "group": "1", "key": "Place", "text": "Places", "description": "Places Entity", "color": "orange", "parent": "thing" },
-    { "group": "1", "key": "Concept", "text": "Concepts", "description": "Concept Entity", "color": "orange", "parent": "thing" },
+    // { "group": "1", "key": "Concept", "text": "Concepts", "description": "Concept Entity", "color": "orange", "parent": "thing" },
     // { "group": "1", "key": "Event", "text": "Events", "description": "Event Entity", "color": "orange", "parent": "thing" },
 
 
+    { "group": "2", "key":"strategy",    "text":"Strategy\nOutcome",  "description":"Strategy Outcome Theme \neg. Retain High Value Customers",  "color":"white",   expanded: true, "parent":"Concept" },
+    { "group": "2", "key":"chain",       "text":"Chain",       "description":"Value Chain Component \neg. Product Development",  "color":"white",  expanded: true, "parent":"strategy" },
+    { "group": "2", "key":"stream",      "text":"Stream",      "description":"Value Stream \neg. Requirement-to-deploy",  "color":"white",  expanded: true, "parent":"chain" },
+    { "group": "2", "key":"activity",    "text":"Activity", "description":"Workflow Activity \neg. Software Development",  "color":"white",  expanded: true, "parent":"stream" },
 
 
+    { "group": "2", "key": "Capability", "text": "Capability","description":"Business Capabiliy \neg. Product Software Development", },
+    
     // { "group": "2", "key": "Tools", "text": "Tools", "color": "DarkViolet" }, 
     { "group": "3", "key": "Technology", "text": "Technology", "description": "Capability Technology", "color": "orange" },
 
@@ -24,10 +29,10 @@ var nodeDataArray = [
     { "key": "Information system services", "text": "Information \nsystem \nservices", "color": "cyan" },
     { "key": "Activity enabling features", "text": "Activity \nenabling \nfeatures", "color": "cyan" },
 
-    // { "key": "SaaS", "text": "SaaS", "color": "cyan" },
-    // { "key": "PIM/CMS", "text": "PIM/CMS", "color": "cyan" },
-    // { "key": "CA/CM", "text": "CA/CM", "color": "cyan" },
-    // { "key": "CRM/CIM", "text": "CRM/CIM", "color": "cyan" },
+    { "key": "SaaS", "text": "SaaS", "color": "cyan" },
+    { "key": "PIM/CMS", "text": "PIM/CMS", "color": "cyan" },
+    { "key": "CA/CM", "text": "CA/CM", "color": "cyan" },
+    { "key": "CRM/CIM", "text": "CRM/CIM", "color": "cyan" },
 
 
     // { "key": "Teams", "text": "Teams", "color": "magenta" },
@@ -39,10 +44,10 @@ var nodeDataArray = [
 
     // { "key": "Staff", "text": "Staff" , "color":"magenta" },
 
-    // { "key": "Position", "text": "Position", "color": "magenta" },
-    // { "key": "Software Developer", "text": "Software Developer", "color": "magenta" },
-    // { "key": "Sales Enginner", "text": "Sales Enginner", "color": "magenta" },
-    // { "key": "Marketing Manager", "text": "Marketing Manager", "color": "magenta" },
+    { "key": "Position", "text": "Position", "color": "magenta" },
+    { "key": "Software Developer", "text": "Software Developer", "color": "magenta" },
+    { "key": "Sales Enginner", "text": "Sales Enginner", "color": "magenta" },
+    { "key": "Marketing Manager", "text": "Marketing Manager", "color": "magenta" },
 
 
     // { "key": "Flows", "text": "Flows", "color": "yellow" },
@@ -52,14 +57,16 @@ var nodeDataArray = [
     { "key": "Action plan checklist", "text": "Action plan checklist", "color": "yellow" },
     { "key": "Logic model indicators", "text": "Logic model indicators", "color": "yellow" },
 
-    // { "key": "SOP", "text": "SOP", "color": "yellow" },
-    // { "key": "Manage Product Data", "text": "Manage Product Data", "color": "yellow" },
-    // { "key": "Manage Customization Content", "text": "Manage Customization Content", "color": "yellow" },
-    // { "key": "Manage Customer Info", "text": "Manage Customer Info", "color": "yellow" },
+    { "key": "SOP", "text": "SOP", "color": "yellow" },
+    { "key": "Manage Product Data", "text": "Manage Product Data", "color": "yellow" },
+    { "key": "Manage Customization Content", "text": "Manage Customization Content", "color": "yellow" },
+    { "key": "Manage Customer Info", "text": "Manage Customer Info", "color": "yellow" },
 
     // { "key": "Activty", "text": "Activty" },
     // { "key": "Group", "text": "Group" },
-    { "key": "Capability", "text": "Capability" }
+    
+    { "group": "0", "key": "Thing", "text": "Things", "description": "Things Entity", "color": "lime" },
+
 
 ]
 
@@ -76,7 +83,7 @@ var linkDataArray = [
     // { "from": "Thing", "to": "Person", "text": "superClassOf", "color": "blue", "thick": 2 },
     // { "from": "Thing", "to": "Organization", "text": "superClassOf", "color": "blue", "thick": 2 },
     // { "from": "Thing", "to": "Place", "text": "superClassOf", "color": "blue", "thick": 2 },
-    { "from": "Thing", "to": "Concept", "text": "superClassOf", "color": "blue", "thick": 2 },
+    // { "from": "Thing", "to": "Concept", "text": "superClassOf", "color": "blue", "thick": 2 },
     // { "from": "Thing", "to": "Event", "text": "superClassOf", "color": "blue", "thick": 2 },
 
 // Teams Tools Flows
@@ -103,6 +110,13 @@ var linkDataArray = [
     // { "from": "Teams", "to": "Tools", "text": "uses" },
 
 
+// Stratey Value Workflow
+    // { "from": "Concept",  "to": "strategy", "text": "superClassOf" },
+    { "from": "strategy", "to": "chain", "text": "isDeliveredBy" },
+    { "from": "chain",    "to": "stream", "text": "isSupportedBy" },
+    { "from": "stream",   "to": "activity", "text": "isAccomplishedBy" },
+    { "from": "activity", "to": "Capability", "text": "isEnabledBy" },
+    
 
 // People Process Technology
 
@@ -136,25 +150,25 @@ var linkDataArray = [
 
 // People Staff Process Tech    
 
-    // { "from": "Software Developer", "to": "Manage Customer Data", "text": "actorIn" },
-    // { "from": "Manage Customer Data", "to": "", "text": "hasActor" },
+    { "from": "Software Developer", "to": "Manage Customer Info", "text": "actorIn" },
+    { "from": "Manage Customer Info", "to": "", "text": "hasActor" },
 
-    // { "from": "Software Developer", "to": "CRM/CIM", "text": "maintains" },
-    // { "from": "CRM/CIM", "to": "Software Developer", "text": "maintainedBy" },
-
-
-    // { "from": "Sales Enginner", "to": "Manage Product Information", "text": "actorIn" },
-    // { "from": "Manage Product Information", "to": "Sales Enginner", "text": "hasActor" },
-
-    // { "from": "Sales Enginner", "to": "PIM/CMS", "text": "updates" },
-    // { "from": "PIM/CMS", "to": "Sales Enginner", "text": "updatedBy" },
+    { "from": "Software Developer", "to": "CRM/CIM", "text": "maintains" },
+    { "from": "CRM/CIM", "to": "Software Developer", "text": "maintainedBy" },
 
 
-    // { "from": "Marketing Manager", "to": "Manage Customization Content", "text": "actorIn" },
-    // { "from": "Manage Customization Content", "to": "Marketing Manager", "text": "hasActor" },
+    { "from": "Sales Enginner", "to": "Manage Product Information", "text": "actorIn" },
+    { "from": "Manage Product Information", "to": "Sales Enginner", "text": "hasActor" },
 
-    // { "from": "Marketing Manager", "to": "CA/CM", "text": "userOf" },
-    // { "from": "CA/CM", "to": "Marketing Manager", "text": "usedBy" },
+    { "from": "Sales Enginner", "to": "PIM/CMS", "text": "updates" },
+    { "from": "PIM/CMS", "to": "Sales Enginner", "text": "updatedBy" },
+
+
+    { "from": "Marketing Manager", "to": "Manage Customization Content", "text": "actorIn" },
+    { "from": "Manage Customization Content", "to": "Marketing Manager", "text": "hasActor" },
+
+    { "from": "Marketing Manager", "to": "CA/CM", "text": "userOf" },
+    { "from": "CA/CM", "to": "Marketing Manager", "text": "usedBy" },
 
 
 
@@ -193,87 +207,87 @@ var linkDataArray = [
 
 
 // instance of people    
-    // { "from": "Position", "to": "People", "text": "instanceOf" },
-    // { "from": "People", "to": "Position", "text": "hasInstacne" },
+    { "from": "Position", "to": "People", "text": "instanceOf" },
+    { "from": "People", "to": "Position", "text": "hasInstacne" },
 
-    // { "from": "Software Developer", "to": "Position", "text": "subclassOf" },
-    // { "from": "Position", "to": "Software Developer", "text": "superclassOf" },
+    { "from": "Software Developer", "to": "Position", "text": "subclassOf" },
+    { "from": "Position", "to": "Software Developer", "text": "superclassOf" },
 
-    // { "from": "Sales Enginner", "to": "Position", "text": "subclassOf" },
-    // { "from": "Position", "to": "Sales Enginner", "text": "superclassOf" },
+    { "from": "Sales Enginner", "to": "Position", "text": "subclassOf" },
+    { "from": "Position", "to": "Sales Enginner", "text": "superclassOf" },
 
-    // { "from": "Marketing Manager", "to": "Position", "text": "subclassOf" },
-    // { "from": "Position", "to": "Marketing Manager", "text": "superclassOf" },
+    { "from": "Marketing Manager", "to": "Position", "text": "subclassOf" },
+    { "from": "Position", "to": "Marketing Manager", "text": "superclassOf" },
 
 
 // instances of processes    
-    // { "from": "SOP", "to": "Process", "text": "instanceOf" },
-    // { "from": "Process", "to": "SaSOPaS", "text": "hasInstacne" },
+    { "from": "SOP", "to": "Process", "text": "instanceOf" },
+    { "from": "Process", "to": "SaSOPaS", "text": "hasInstacne" },
 
-    // { "from": "Manage Product Data", "to": "SOP", "text": "subclassOf" },
-    // { "from": "SOP", "to": "Manage Product Data", "text": "superclassOf" },
+    { "from": "Manage Product Data", "to": "SOP", "text": "subclassOf" },
+    { "from": "SOP", "to": "Manage Product Data", "text": "superclassOf" },
 
-    // { "from": "Manage Customization Content", "to": "SOP", "text": "subclassOf" },
-    // { "from": "SOP", "to": "Manage Customization Content", "text": "superclassOf" },
+    { "from": "Manage Customization Content", "to": "SOP", "text": "subclassOf" },
+    { "from": "SOP", "to": "Manage Customization Content", "text": "superclassOf" },
 
-    // { "from": "Manage Customer Info", "to": "SOP", "text": "subclassOf" },
-    // { "from": "SOP", "to": "Manage Customer Info", "text": "superclassOf" },
+    { "from": "Manage Customer Info", "to": "SOP", "text": "subclassOf" },
+    { "from": "SOP", "to": "Manage Customer Info", "text": "superclassOf" },
 
 
 // instance of technology
-    // { "from": "SaaS", "to": "Technology", "text": "instanceOf" },
-    // { "from": "Technology", "to": "SaaS", "text": "hasInstacne" },
+    { "from": "SaaS", "to": "Technology", "text": "instanceOf" },
+    { "from": "Technology", "to": "SaaS", "text": "hasInstacne" },
 
-    // { "from": "CRM/CIM", "to": "SaaS", "text": "subclassOf" },
-    // { "from": "SaaS", "to": "CRM/CIM", "text": "superclassOf" },
+    { "from": "CRM/CIM", "to": "SaaS", "text": "subclassOf" },
+    { "from": "SaaS", "to": "CRM/CIM", "text": "superclassOf" },
 
-    // { "from": "CA/CM", "to": "SaaS", "text": "subclassOf" },
-    // { "from": "SaaS", "to": "SaaS", "text": "superclassOf" },
+    { "from": "CA/CM", "to": "SaaS", "text": "subclassOf" },
+    { "from": "SaaS", "to": "SaaS", "text": "superclassOf" },
 
-    // { "from": "PIM/CMS", "to": "SaaS", "text": "subclassOf" },
-    // { "from": "SaaS", "to": "PIM/CMS", "text": "superclassOf" },
+    { "from": "PIM/CMS", "to": "SaaS", "text": "subclassOf" },
+    { "from": "SaaS", "to": "PIM/CMS", "text": "superclassOf" },
 
 
 
 // people process and technology relationships
-    // { "from": "CRM/CIM", "to": "Manage Customer Data", "text": "dataFor" },
-    // { "from": "Manage Customer Data", "to": "CRM/CIM", "text": "dataFrom" },
+    { "from": "CRM/CIM", "to": "Manage Customer Data", "text": "dataFor" },
+    { "from": "Manage Customer Data", "to": "CRM/CIM", "text": "dataFrom" },
 
-    // { "from": "CA/CM", "to": "Manage Customization Content", "text": "dataFor" },
-    // { "from": "Manage Customization Content", "to": "CA/CM", "text": "dataFrom" },
+    { "from": "CA/CM", "to": "Manage Customization Content", "text": "dataFor" },
+    { "from": "Manage Customization Content", "to": "CA/CM", "text": "dataFrom" },
 
-    // { "from": "PIM/CMS", "to": "Manage Product Information", "text": "dataFor" },
-    // { "from": "Manage Product Information", "to": "PIM/CMS", "text": "dataFrom" },
-
-
-    // { "from": "Process workflow map", "to": "Job Position description", "text": "defines" },
-    // { "from": "Job Position description", "to": "Process workflow map", "text": "definedBy" },
-
-    // { "from": "Logic model indicators", "to": "Career path latticex", "text": "monitors" },
-    // { "from": "Career path latticex", "to": "Logic model indicators", "text": "monitoredBy" },
-
-    // { "from": "Action plan checklist", "to": "Skill behavior matrix", "text": "requires" },
-    // { "from": "Skill behavior matrix", "to": "Action plan checklist", "text": "requiredBy" },
+    { "from": "PIM/CMS", "to": "Manage Product Information", "text": "dataFor" },
+    { "from": "Manage Product Information", "to": "PIM/CMS", "text": "dataFrom" },
 
 
-    // { "from": "Activity enabling features", "to": "Action plan checklist", "text": "executes" },
-    // { "from": "Action plan checklist", "to": "Activity enabling features", "text": "executedWith" },
+    { "from": "Process workflow map", "to": "Job Position description", "text": "defines" },
+    { "from": "Job Position description", "to": "Process workflow map", "text": "definedBy" },
 
-    // { "from": "Technology components", "to": "Process workflow map", "text": "enables" },
-    // { "from": "Process workflow map", "to": "Technology components", "text": "enabledBy" },
+    { "from": "Logic model indicators", "to": "Career path latticex", "text": "monitors" },
+    { "from": "Career path latticex", "to": "Logic model indicators", "text": "monitoredBy" },
 
-    // { "from": "Information system services", "to": "Logic model indicators", "text": "dataFor" },
-    // { "from": "Logic model indicators", "to": "Information system services", "text": "dataFrom" },
+    { "from": "Action plan checklist", "to": "Skill behavior matrix", "text": "requires" },
+    { "from": "Skill behavior matrix", "to": "Action plan checklist", "text": "requiredBy" },
 
 
-    // { "from": "Activity enabling features", "to": "Job Position description", "text": "assists" },
-    // { "from": "Job Position description", "to": "Activity enabling features", "text": "assistedBy" },
+    { "from": "Activity enabling features", "to": "Action plan checklist", "text": "executes" },
+    { "from": "Action plan checklist", "to": "Activity enabling features", "text": "executedWith" },
 
-    // { "from": "Technology components", "to": "Career path latticex", "text": "enables" },
-    // { "from": "Career path latticex", "to": "Technology components", "text": "enabledBy" },
+    { "from": "Technology components", "to": "Process workflow map", "text": "enables" },
+    { "from": "Process workflow map", "to": "Technology components", "text": "enabledBy" },
 
-    // { "from": "Information system services", "to": "Skill behavior matrix", "text": "dataFor" },
-    // { "from": "Skill behavior matrix", "to": "Information system services", "text": "dataFrom" },
+    { "from": "Information system services", "to": "Logic model indicators", "text": "dataFor" },
+    { "from": "Logic model indicators", "to": "Information system services", "text": "dataFrom" },
+
+
+    { "from": "Activity enabling features", "to": "Job Position description", "text": "assists" },
+    { "from": "Job Position description", "to": "Activity enabling features", "text": "assistedBy" },
+
+    { "from": "Technology components", "to": "Career path latticex", "text": "enables" },
+    { "from": "Career path latticex", "to": "Technology components", "text": "enabledBy" },
+
+    { "from": "Information system services", "to": "Skill behavior matrix", "text": "dataFor" },
+    { "from": "Skill behavior matrix", "to": "Information system services", "text": "dataFrom" },
 
 
     { "from": "Process workflow map", "to": "Action plan checklist", "text": "hasSameRole", "description": "Process Map Role sameAs Action Checklist Role" },
@@ -304,7 +318,7 @@ var linkDataArray = [
 
 
 
-// {"from":"Thing", "to":"capability", "text":"parent of", "color":"blue", "thick":2},
+// {"from":"thing", "to":"capability", "text":"parent of", "color":"blue", "thick":2},
 
 // {"from":"capability", "to":"competency", "text":"parent of"},
 // {"from":"competency", "to":"position", "text":"parent of"},
@@ -323,3 +337,4 @@ var linkDataArray = [
 
 
     ]
+
